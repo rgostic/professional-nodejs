5b2b4fdd87eff564852f5fffd0a27b4279fe02bf
rgostic <5856138+rgostic@users.noreply.github.com>
2019-07-07 21:50:42 -0400

    Ch8 complete

---
+++ ch8-processes/client.js
@@ -0,0 +1,18 @@
+var spawn = require('child_process').spawn;
+
+var child = spawn('node', ['./ch8-processes/index.js']);
+
+setInterval(function() {
+  var number = Math.floor(Math.random() * 10000);
+  child.stdin.write(number + '\n');
+
+  child.stdout.once('data', function(data) {
+    console.log('child replied to ' + number + ' with: ' + data);
+  });
+
+
+}, 1000);
+
+child.stderr.on('data', function(data) {
+  process.stdout.write(data);
+});
