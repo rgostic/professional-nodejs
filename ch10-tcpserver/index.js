36d722f38da83a1ff0eb340a1141cd54bc75d6d4
rgostic <5856138+rgostic@users.noreply.github.com>
2019-07-08 18:55:13 -0400

    ch10,11,13,18,22

--- ch10-tcpserver/index.js
+++ ch10-tcpserver/index.js
@@ -1,1 +1,23 @@
-index.js
+var server = require('net').createServer();
+
+var port = 4001;
+
+server.on('listening', function() {
+  console.log('server listening on port ' + port);
+});
+
+server.on('connection', function(socket) {
+  console.log('server has a new connection');
+  socket.end();
+  server.close();
+})
+
+server.on('close', function() {
+  console.log('server closed');
+});
+
+server.on('error', function(err) {
+  console.log('error :' . err.message);
+});
+
+server.listen(port);
---
+++ ch18-debugging/index.js
@@ -0,0 +1,13 @@
+var a = 0;
+
+function init() {
+  a = 1;
+}
+
+function incr() {
+  var a = a + 1;
+}
+
+init();
+
+incr();
